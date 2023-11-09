b5dea1b98296c9e43c202f60e80358c537be5790
Dan <daniel@fail.ie>
2023-11-09 20:03:17 +0000

    Add portainer, update deploy to regen dashboard

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,14 @@ fi
 arg=$1
 cmd="ansible-playbook docker.ansible.yml"
 
+./dashboard_gen.py
+
 if [[ "$arg" != "all" ]] then
     cmd="$cmd --extra-vars 'single=$arg'"
+fi
+
+if [[ "$arg" == "Homer" ]] then
+    cmd="$cmd --extra-vars 'single=HomerRemote' && $cmd --extra-vars 'single=HomerLocal'"
 fi
 
 eval $cmd
